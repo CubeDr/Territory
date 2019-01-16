4b7acadf5ff8e68ed96bfb976f1e4140e2d6a163
CubeDr <spaceship00@naver.com>
2019-01-16 11:30:17 +0900

    ADD: Apply pub-sub pattern to Engine
    Engine roles as event bus.
    Publisher can publish event with emit().
    Subscriber can subscribe event with on().

--- public/javascripts/scenes/engine-scene.js
+++ public/javascripts/scenes/engine-scene.js
@@ -1,6 +1,9 @@
 class Engine extends Phaser.Scene {
     constructor() {
         super({key: 'engine'});
+
+        // propery to handle events
+        this._events = {};
     }
 
     init(player) {
@@ -10,4 +13,17 @@ class Engine extends Phaser.Scene {
     create() {
         this.scene.launch('territory', player.territories[0]);
     }
+
+    /* ===== PubSub Pattern methods ===== */
+    on(eventName, callback) {
+        if(this._events[eventName] == null)
+            this._events[eventName] = [];
+        this._events[eventName].push(callback);
+    }
+
+    emit(eventName, data) {
+        if(this._events[eventName])
+            this._events[eventName].forEach((callback) => callback(data));
+    }
+    /* =================================== */
 }
