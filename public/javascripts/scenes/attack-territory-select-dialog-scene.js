96ab95507929ee966eb829d60f714fe2249d62e6
CubeDr <spaceship00@naver.com>
2019-01-17 17:42:46 +0900

    ADD: Set AttackTerritorySelectDialog modal
    It consumes all the events.

--- public/javascripts/scenes/attack-territory-select-dialog-scene.js
+++ public/javascripts/scenes/attack-territory-select-dialog-scene.js
@@ -5,7 +5,26 @@ class AttackTerritorySelectDialogScene extends Phaser.Scene {
         super({key: AttackTerritorySelectDialogScene.KEY});
     }
 
+    preload() {
+
+    }
+
     create() {
-        console.log('launched');
+        let g = this.add.graphics();
+        g.fillStyle(0x000000, 0.7);
+        g.fillRect(0, IMAGE_HEIGHT, GAME_WIDTH, CAMERA_HEIGHT);
+
+        let textX = CAMERA_WIDTH / 2;
+        let textY = CAMERA_HEIGHT / 4;
+        // title text
+        this.add.text(textX, textY, "병력을 출진시킬 영지를 선택하세요")
+            .setOrigin(0.5, 1)
+        // set dialog modal
+            .setInteractive(new Phaser.Geom.Rectangle(), () => { return true; })
+            .on('pointerdown',  (p, x, y, e) => e.stopPropagation())
+            .on('pointerup',    (p, x, y, e) => e.stopPropagation())
+            .on('pointermove',  (p, x, y, e) => e.stopPropagation())
+            .on('pointerover',  (p, x, y, e) => e.stopPropagation())
+            .on('pointerout',   (p, e) => e.stopPropagation());
     }
 }
