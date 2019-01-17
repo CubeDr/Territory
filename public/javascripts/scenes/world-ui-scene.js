61e1e851a3330f445b8c296f887b339d0d76e614
CubeDr <spaceship00@naver.com>
2019-01-17 13:06:18 +0900

    ADD: Add touch logic to territory buttons and scene itself
    Buttons react to
     - pointer down
     - pointer up
     - pointer over
     - pointer out
    
    Scene can listen to
     - pointer down in scene
     - pointer up when pointer was down in the scene
     - pointer move when pointer was down in the scene

--- public/javascripts/scenes/world-ui-scene.js
+++ public/javascripts/scenes/world-ui-scene.js
@@ -5,6 +5,7 @@ class WorldUIScene extends Phaser.Scene {
         this.player = null;
         this.currentHover = null;
         this.isScrolling = false;
+        this.isPointerDown = false;
     }
 
     preload() {
@@ -17,9 +18,18 @@ class WorldUIScene extends Phaser.Scene {
     }
 
     create() {
-        this.cameras.main.setSize(GAME_WIDTH - CAMERA_WIDTH, CAMERA_HEIGHT);
-        this.cameras.main.setPosition(CAMERA_WIDTH, 100);
+        this.x = CAMERA_WIDTH;
+        this.y = 100;
+        this.width = GAME_WIDTH - CAMERA_WIDTH;
+        this.height = CAMERA_HEIGHT;
+        this.cameras.main.setSize(this.width, this.height);
+        this.cameras.main.setPosition(this.x, this.y);
 
+        this._createTerritoryButtons();
+        this._attachMainTouchListener();
+    }
+
+    _createTerritoryButtons() {
         let offsetX = IMAGE_WIDTH / 2;
         let offsetY = IMAGE_HEIGHT / 2;
         this.player.territories.forEach((t) => {
@@ -30,8 +40,56 @@ class WorldUIScene extends Phaser.Scene {
         });
     }
 
+    _attachMainTouchListener() {
+        let self = this;
+        this.input
+            .on('pointerdown', (p) => {
+                if(this._outOfCamera(p)) return;
+                self.isPointerDown = true;
+                console.log("D: " + p.x + ", " + p.y);
+            })
+            .on('pointerup', (p) => {
+                if(!self.isPointerDown) return;
+                self.isPointerDown = false;
+                console.log("U: " + p.x + ", " + p.y);
+            })
+            .on('pointermove', (p) => {
+                if(!self.isPointerDown) return;
+                console.log("M: " + p.x + ", " + p.y);
+            });
+    }
+
     _addTerritoryButton(territory, x, y) {
-        this.add.image(x, y, 'button').setScale(0.95);
-        this.add.image(x, y, 'w_territory').setScale(0.85);
+        let c = this.add.container(x, y);
+        c.add(this.add.image(0, 0, 'button').setScale(0.95));
+        c.add(this.add.image(0, 0, 'w_territory').setScale(0.85));
+        c.territory = territory;
+
+        let self = this;
+        let width = IMAGE_WIDTH * .95;
+        let height = IMAGE_HEIGHT * .95;
+        c.setInteractive(
+            new Phaser.Geom.Rectangle(-width/2, -height/2, width, height),
+            Phaser.Geom.Rectangle.Contains)
+            .on('pointerover', () => {
+                self.currentHover = c;
+                c.iterate((c) => c.setTint(0xaaaaaa));
+            })
+            .on('pointerout', () => {
+                self.currentHover = null;
+                c.iterate((c) => c.clearTint());
+            })
+            .on('pointerdown', () => {
+                if(self.currentHover !== c) return;
+                c.iterate((c) => c.setTint(0x999999));
+            })
+            .on('pointerup', () => {
+                if(self.currentHover !== c) return;
+                c.iterate((c) => c.setTint(0xaaaaaa));
+            });
+    }
+
+    _outOfCamera(p) {
+        return p.x < this.x || p.y < this.y || p.x > this.x + this.width || p.y > this.y + this.height;
     }
 }
