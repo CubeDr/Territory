7779f989a4fdbb54e91bd59aa7f14e42e6ccab95
CubeDr <spaceship00@naver.com>
2019-01-22 18:53:48 +0900

    ADD: Show territory name on territory enter dialog

--- public/javascripts/scenes/dialogs/territory-enter-dialog.js
+++ public/javascripts/scenes/dialogs/territory-enter-dialog.js
@@ -3,7 +3,7 @@ class TerritoryEnterDialog extends Phaser.GameObjects.Container {
         super(scene, config);
         this.scene = scene;
         this.width = 200;
-        this.height = 80;
+        this.height = 120;
 
         this.territory = null;
 
@@ -15,24 +15,29 @@ class TerritoryEnterDialog extends Phaser.GameObjects.Container {
         });
         this.add(background);
 
+        let title = scene.add.text(0, 0, '영지(0, 0)', {fontSize: 20}).setOrigin(0.5, 0.5);
+        this.add(title);
+        title.setPosition(0, -35);
+        this.titleText = title;
+
         let quantityIcon = scene.add.image(0, 0, 'quantity');
         this.add(quantityIcon);
-        quantityIcon.setPosition(-70, -10);
+        quantityIcon.setPosition(-70, 0);
 
         let quantityText = scene.add.text(0, 0, '0', {fontSize: 20});
         this.add(quantityText);
         quantityText.setOrigin(0, 0.5);
-        quantityText.setPosition(-50, -10);
+        quantityText.setPosition(-50, 0);
         this.quantityText = quantityText;
 
         let qualityIcon = scene.add.image(0, 0, 'quality');
         this.add(qualityIcon);
-        qualityIcon.setPosition(15, -10);
+        qualityIcon.setPosition(15, 0);
 
         let qualityText = scene.add.text(0, 0, '0', {fontSize: 20});
         this.add(qualityText);
         qualityText.setOrigin(0, 0.5);
-        qualityText.setPosition(35, -10);
+        qualityText.setPosition(35, 0);
         this.qualityText = qualityText;
 
         let enterButton = new TextButton(scene, 0, 0, '입장', {
@@ -43,12 +48,13 @@ class TerritoryEnterDialog extends Phaser.GameObjects.Container {
         });
         this.add(enterButton);
         enterButton.setOrigin(0.5);
-        enterButton.setPosition(0, 25);
+        enterButton.setPosition(0, 40);
         this.setVisible(false);
     }
 
     update() {
         if(!this.territory) return;
+        this.titleText.setText("영지(" + this.territory.x + ", " + this.territory.y + ")");
         this.quantityText.setText(parseInt(this.territory.army.quantity));
         this.qualityText.setText(parseInt(this.territory.army.quality));
     }
