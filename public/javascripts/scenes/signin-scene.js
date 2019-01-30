502ae55d78dcb01fd3145f046a01ee69b217a455
CubeDr <spaceship00@naver.com>
2019-01-30 18:36:41 +0900

    ADD: Add sign in scene design
    Add background, title text.
    Position google sign in button.

--- public/javascripts/scenes/signin-scene.js
+++ public/javascripts/scenes/signin-scene.js
@@ -2,4 +2,19 @@ class SigninScene extends Phaser.Scene {
     constructor() {
         super({key: 'signin'});
     }
+
+    preload() {
+        this.load.image('background', 'assets/signin/awesome_background.jpg');
+    }
+
+    create() {
+        this.add.image(0, 0, 'background').setOrigin(0);
+        this.add.rectangle(450, 200, 900, 200, 0).setAlpha(0.7);
+        this.add.text(450, 200, 'TERRITORY', {
+            fontSize: 70
+        }).setOrigin(0.5);
+        this.add.text(900, 900, '배경: 구글에 awesome background 검색ㅎ', {
+            color: '#999999'
+        }).setOrigin(1, 1);
+    }
 }
