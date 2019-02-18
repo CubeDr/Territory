5d5d895a9bc950bc618209a69b1041eb564af3d8
CubeDr <spaceship00@naver.com>
2019-02-18 17:35:09 +0900

    ADD: Show list of territories in AttackTerritorySelectDialog

--- public/javascripts/scenes/dialogs/attack-territory-select-dialog.js
+++ public/javascripts/scenes/dialogs/attack-territory-select-dialog.js
@@ -13,6 +13,10 @@ class AttackTerritorySelectDialog extends Phaser.Scene {
     preload() {
         this.load.image('background_dialog', 'assets/background_dialog.png');
         this.load.image('item_back', 'assets/ui/list_item.png');
+        this.load.image('item_back_selected', 'assets/ui/list_item_selected.png');
+        this.load.image('icon', 'assets/ui/territory_icon.png');
+        this.load.image('quantity_icon', 'assets/ui/resources/icon_quantity.png');
+        this.load.image('quality_icon', 'assets/ui/resources/icon_quality.png');
     }
 
     create () {
@@ -30,17 +34,26 @@ class AttackTerritorySelectDialog extends Phaser.Scene {
         this.add.existing(new TextButton(this, 420, 710, '확인', {
             fontSize: 20,
             onClick: () => {
-                this.scene.remove(AttackTerritorySelectDialog.KEY);
+                this.close();
             }
         }));
         this.add.existing(new TextButton(this, 500, 710, '취소', {
             fontSize: 20,
             onClick: () => {
-                this.scene.remove(AttackTerritorySelectDialog.KEY);
+                this.close();
             }
         }));
 
         this.list = this.createTerritoryList();
+    }
+
+    close() {
+        this.list.iterate((item) => {
+            gameEngine
+                .off('changeQuantity', item.quantityListener)
+                .off('changeQuality', item.qualityListener);
+        });
+        this.scene.remove(AttackTerritorySelectDialog.KEY);
     }
 
     createTerritoryList() {
@@ -51,12 +64,96 @@ class AttackTerritorySelectDialog extends Phaser.Scene {
         let list = this.add.container(215, 310);
         list.mask = g.createGeometryMask();
 
+        let y = 5;
+        gameEngine.player.territories.forEach((t) => {
+            let item = this.createTerritoryItem(t);
+            item.setPosition(item.x, y);
+            list.add(item);
+            y += 80;
+        });
+
         g.setInteractive(new Phaser.Geom.Rectangle(210, 310, 380, 390), Phaser.Geom.Rectangle.Contains)
             .on('pointerdown', this.pointerDown, this)
             .on('pointerup', this.pointerUp, this)
             .on('pointermove', this.pointerMove, this);
 
         return list;
+    }
+
+    createTerritoryItem(t) {
+        let item = this.add.container();
+        item.territory = t;
+
+        item.back = this.add.nineslice(0, 0, 370, 75, 'item_back', 30, 10).setOrigin(0);
+        item.add(item.back);
+
+        item.backSel = this.add.nineslice(0, 0, 370, 75, 'item_selected', 30, 10).setOrigin(0);
+        item.add(item.backSel);
+        item.backSel.visible = false;
+
+        let icon = this.add.image(8, 8, 'icon').setOrigin(0);
+        item.add(icon);
+
+        let text = this.add.text(74, 20, '영지(' + t.x + ", " + t.y + ")")
+            .setOrigin(0, 0.5);
+        item.add(text);
+
+        let quantityIcon = this.add.image(74, 36, 'quantity_icon').setOrigin(0);
+        item.add(quantityIcon);
+
+        let quantityText = this.add.text(109, 51, Math.floor(t.army.quantity), {fontSize:20}).setOrigin(0, 0.5);
+        item.add(quantityText);
+
+        let qualityIcon = this.add.image(177, 36, 'quality_icon').setOrigin(0);
+        item.add(qualityIcon);
+
+        let qualityText = this.add.text(212, 51, Math.floor(t.army.quality), {fontSize:20}).setOrigin(0, 0.5);
+        item.add(qualityText);
+
+        item.quantityListener = (t) => {
+            if(t !== item.territory) return;
+            quantityText.setText(Math.floor(t.army.quantity));
+        };
+        item.qualityListener = (t) => {
+            if(t !== item.territory) return;
+            qualityText.setText(Math.floor(t.army.quality));
+        };
+        this.scene.get('engine')
+            .on('changeQuantity', item.quantityListener)
+            .on('changeQuality', item.qualityListener);
+
+        item.setInteractive(new Phaser.Geom.Rectangle(0, 0, 370, 75), Phaser.Geom.Rectangle.Contains)
+            .on('pointerdown', (p) => {
+                this.pointerDown(p);
+            }, this)
+            .on('pointerup', (p) => {
+                if(!this.isScrolling) {
+                    if(!this._isInBody(p.x, p.y) || this.pointer.down==null) return;
+                    if(this.selected === item) {
+                        // unselect
+                        this.selected = null;
+                        item.back.visible = true;
+                        item.backSel.visible = false;
+                    } else if(this.selected != null) {
+                        // unselect
+                        this.selected.back.visible = true;
+                        this.selected.backSel.visible = false;
+                        // select
+                        this.selected = item;
+                        this.selected.back.visible = false;
+                        this.selected.backSel.visible = true;
+                    } else {
+                        // select
+                        this.selected = item;
+                        item.back.visible = false;
+                        item.backSel.visible = true;
+                    }
+                }
+                this.pointerUp(p);
+            }, this)
+            .on('pointermove', this.pointerMove, this);
+
+        return item;
     }
 
     _isInBody(x, y) {
