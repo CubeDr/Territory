faf85cfd13d5b94ae6d3b34a1577904dbcb62dec
CubeDr <spaceship00@naver.com>
2019-02-20 11:47:03 +0900

    ADD: Show territory dialog on hover

--- public/javascripts/scenes/fight/fight-scene.js
+++ public/javascripts/scenes/fight/fight-scene.js
@@ -7,13 +7,14 @@ class FightScene extends Phaser.Scene {
         this.cameraCenter = {};
 
         // prepare
-        this.state = 0;
+        this.state = -2;
     }
 
     preload() {
         loadTileSprites(this, ["grass"]);
         this.load.image('territory', 'assets/world/tile_territory.png');
         this.load.image('tile_food', 'assets/menu/menu_food.png');
+        this.load.image('dialog', 'assets/background_dialog.png');
     }
 
     init(config) {
@@ -52,9 +53,21 @@ class FightScene extends Phaser.Scene {
         this.cameras.main.setSize(800, 800);
         this.cameras.main.setBackgroundColor('#000000');
         this.add.text(100, 100, "ASDFASDFASDFSADF");
+
+        // territory dialog
+        let d = this.add.container();
+        d.setDepth(1);
+
+        let back = this.add.nineslice(0, 0, 200, 100, 'dialog', 10, 10);
+        d.add(back);
+
+        this.territoryDialog = d;
+
+        this.doCreate();
     }
 
     doCreate() {
+        if(++this.state < 0) return;
         for(let ty=this.boundary.minY; ty<=this.boundary.maxY; ty++) {
             for(let tx=this.boundary.minX; tx<=this.boundary.maxX; tx++) {
                 let x = tx * 100;
@@ -75,6 +88,19 @@ class FightScene extends Phaser.Scene {
             let tile = this.add.image(x, y, t.quantity>0?'territory':'tile_food');
             t.tile = tile;
             tile.territory = t;
+
+            tile.setInteractive()
+                .on('pointerover', () => {
+                    this.territoryDialog.visible = true;
+                })
+                .on('pointermove', (p, lx, ly) => {
+                    let x = tile.x + lx - tile.width/2;
+                    let y = tile.y + ly - tile.height/2;
+                    this.territoryDialog.setPosition(x, y);
+                })
+                .on('pointerout', () => {
+                    this.territoryDialog.visible = false;
+                });
         });
     }
 
