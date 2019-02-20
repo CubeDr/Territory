d8fdea90dfbcbc39ec7371ed3fe81bb9613505e4
CubeDr <spaceship00@naver.com>
2019-02-20 11:06:46 +0900

    ADD: Add grass tile

--- public/javascripts/scenes/fight/fight-scene.js
+++ public/javascripts/scenes/fight/fight-scene.js
@@ -4,8 +4,16 @@ class FightScene extends Phaser.Scene {
     constructor() {
         super({KEY: FightScene.KEY});
 
+        this.cameraCenter = {};
+
         // prepare
         this.state = 0;
+    }
+
+    preload() {
+        loadTileSprites(this, ["grass"]);
+        this.load.image('territory', 'assets/world/tile_territory.png');
+        this.load.image('food', 'assets/menu/menu_food.png');
     }
 
     init(config) {
@@ -30,13 +38,38 @@ class FightScene extends Phaser.Scene {
                 boundary.minY--;
             }
             console.log(boundary);
+            this.boundary = boundary;
+            this.doCreate();
         });
     }
 
     create() {
+        createAnimations(this, ['grass']);
+
         this.cameras.main.setPosition(0, 100);
         this.cameras.main.setSize(800, 800);
         this.cameras.main.setBackgroundColor('#000000');
         this.add.text(100, 100, "ASDFASDFASDFSADF");
+    }
+
+    doCreate() {
+        for(let ty=this.boundary.minY; ty<=this.boundary.maxY; ty++) {
+            for(let tx=this.boundary.minX; tx<=this.boundary.maxX; tx++) {
+                let x = tx * 100;
+                let y = ty * 100;
+                let t = this.add.sprite(x, y, 'grassSprite');
+                t.anims.load('grassAnim');
+                t.anims.play('grassAnim');
+            }
+        }
+        let cx = this.boundary.minX + this.boundary.maxX;
+        let cy = this.boundary.minY + this.boundary.maxY;
+        this.centerOn(cx * 100 ,cy * 100);
+    }
+
+    centerOn(x, y) {
+        this.cameraCenter.x = x;
+        this.cameraCenter.y = y;
+        this.cameras.main.centerOn(x, y);
     }
 }
